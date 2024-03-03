49e26ce707311d3bb299ade9b517033b57329435
gooseknight <coppertrin@gmail.com>
2024-03-03 16:52:13 +0700

    Update rider_account.py

--- internals/rider_account.py
+++ internals/rider_account.py
@@ -2,14 +2,20 @@ from internals.account import Account, Profile
 from internals.order import Order
 
 
-class RiderAccount(Account):
-    def __init__(self, account_id: str, password: str, profile: Profile, recieve_order_list, reviewed_list):
+class RiderAccount(Account) :
+    def __init__(self,account_id : str, password : str, profile = None):
         super().__init__(account_id, password, profile)
-        self.__recieve_order_list = recieve_order_list
-        self.__reviewed_list = reviewed_list
-
-    def add_order(self, order: Order):
+        self.__recieve_order_list = []
+        self.__review_list = []
+    #getter
+    @property
+    def recieve_order_list(self):
+        return self.__recieve_order_list
+    #setter
+    @recieve_order_list.setter
+    def recieve_order_list(self, order):
         self.__recieve_order_list.append(order)
 
-    def remove_order(self, order: Order):
-        self.__recieve_order_list.remove(order)
+    #method
+    def get_order_list(self):
+        return self.__recieve_order_list
