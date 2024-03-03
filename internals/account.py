bc302d3fa979fa26b76124fea83d08a163f6c825
gooseknight <coppertrin@gmail.com>
2024-03-03 16:26:10 +0700

    Update account.py
    add search_order_by_id method

--- internals/account.py
+++ internals/account.py
@@ -2,34 +2,44 @@ from internals.profile import Profile
 
 
 class Account:
-    def __init__(self, account_id: str, password: str, profile: Profile):
+    def __init__(self, account_id: str, password: str, profile):
         self.__account_id = account_id
         self.__password = password
         self.__profile = profile
 
     def get_name(self):
         return self.__profile.username
-
+    #getter
     @property
     def account_id(self):
         return self.__account_id
-
-    @account_id.setter
-    def account_id(self, account_id):
-        self.__account_id = account_id
 
     @property
     def password(self):
         return self.__password
 
+    @property
+    def profile(self):
+        return self.__profile
+    #setter
+    @account_id.setter
+    def account_id(self, account_id):
+        self.__account_id = account_id
+
     @password.setter
     def password(self, password):
         self.__password = password
 
-    @property
-    def profile(self):
-        return self.__profile
-
     @profile.setter
     def profile(self, profile):
         self.__profile = profile
+    #method
+    def add_profile(self, profile):
+        self.__profile = profile
+        
+    def search_order_by_id(self, order_id : str):
+        for order in self.get_order_list():
+            if order.order_id == order_id:
+                print(order.order_id)
+                return order
+        return "Order not found"
