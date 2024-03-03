9d103e5c8f5b70c2d3a9ab95a6960c5074007713
gooseknight <coppertrin@gmail.com>
2024-03-03 15:46:57 +0700

    Update order.py
    add getter and setter

--- internals/order.py
+++ internals/order.py
@@ -1,13 +1,47 @@
-from internals.payment import Payment
-
-
 class Order:
-    def __init__(self, order_id, customer, rider, customer_address, order_status, payment: Payment):
+    def __init__(self,order_id = None, customer = None,customer_address = None, rider = None, restaurant_list = [], food_list = [], order_state = None, payment = None) :
         self.__order_id = order_id
         self.__customer = customer
         self.__customer_address = customer_address
-        self.__restaurant_list = []
-        self.__food_list = []
         self.__rider = rider
-        self.__order_status = order_status
+        self.__restaurant_list = restaurant_list
+        self.__food_list = food_list
+        self.__order_state = order_state
         self.__payment = payment
+    #getter
+    @property
+    def order_id(self):
+        return self.__order_id
+    
+    @property
+    def customer(self):
+        return self.__customer
+    
+    @property
+    def customer_address(self):
+        return self.__customer_address
+    
+    @property
+    def rider(self):
+        return self.__rider
+    
+    @property
+    def restaurant_list(self):
+        return self.__restaurant_list
+    
+    @property
+    def food_list(self):
+        return self.__food_list
+    
+    @property
+    def order_state(self):
+        return self.__order_state
+    
+    @property
+    def payment(self):
+        return self.__payment
+    #setter
+    @order_state.setter
+    def order_state(self, new_order_state: str):
+        self.__order_state = new_order_state
+    
