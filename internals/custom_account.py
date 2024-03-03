b1894444fb3840a191f67e25ea58f3a870a264a3
gooseknight <coppertrin@gmail.com>
2024-03-03 16:40:32 +0700

    Update custom_account.py

--- internals/custom_account.py
+++ internals/custom_account.py
@@ -3,7 +3,7 @@ from internals.account import Account, Profile
 
 class CustomerAccount(Account) :
     def __init__(self, account_id: str, password: str, profile = None):
-        super().__init__(account_id, password)
+        super().__init__(account_id, password, profile)
         self.__address_list = []
         self.__reviewed_list = []
         self.__current_order = []
