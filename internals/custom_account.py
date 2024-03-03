323e0751534a8a41739ab80ca25366f50f19e00b
gooseknight <coppertrin@gmail.com>
2024-03-03 16:37:28 +0700

    Update custom_account.py

--- internals/custom_account.py
+++ internals/custom_account.py
@@ -1,26 +1,23 @@
 from internals.account import Account, Profile
 
 
-class CustomerAccount(Account):
-    def __init__(self, account_id: str, password: str, profile: Profile):
-        if not isinstance(profile, Profile):
-            ValueError("Error")
-        super().__init__(account_id, password, profile)
+class CustomerAccount(Account) :
+    def __init__(self, account_id: str, password: str, profile = None):
+        super().__init__(account_id, password)
         self.__address_list = []
         self.__reviewed_list = []
-        self.__current_order = None
-        self.__order_list = []
-
+        self.__current_order = []
+    
+    #getter
     @property
     def current_order(self):
         return self.__current_order
     
-    @property
-    def order_list(self):
-        return self.__order_list
+    #setter
+    @current_order.setter
+    def current_order(self, order):
+        self.__current_order = order
 
-    def add_address(self, address: str):
-        self.__address_list.append(address)
-
-    def remove_address(self, address: str):
-        self.__address_list.remove(address)
+    #method
+    def get_order_list(self):
+        return self.__current_order
