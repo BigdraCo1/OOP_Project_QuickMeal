056beb91e182e935e7415af96f0f36a2c8ac8b31
gooseknight <coppertrin@gmail.com>
2024-03-05 01:25:24 +0700

    Edit Show_detail API

--- routers/cancel.py
+++ routers/cancel.py
@@ -4,13 +4,13 @@ from fastapi import APIRouter, HTTPException, status
 
 app = APIRouter()
 
-@app.post("/cancel_order", tags = ["Orders"])
+@app.post("/cancel_order", tags = ["Cancel"])
 async def cancel_order(account_id: str, order_id: str) -> dict:
     return {
         "data": database.system.customer_cancel_order(account_id, order_id)
     }
 
-@app.post("/cancel_food", tags = ["Orders"])
+@app.post("/cancel_food", tags = ["Cancel"])
 async def cancel_food(restaurant_account_id: str, order_id: str, food_name: str, string: str) -> dict:
     return {
         "data": database.system.restaurant_cancel_order(restaurant_account_id, order_id, food_name, string)
