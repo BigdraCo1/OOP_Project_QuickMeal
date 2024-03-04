e5c851bae166170f1d8723e32c1a54ee1ce62a0a
gooseknight <coppertrin@gmail.com>
2024-03-04 21:22:05 +0700

    Add api.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import menu, search, restaurant
+from routers import menu, search, restaurant, order_detail, cancel
 
 app = FastAPI()
 
@@ -16,3 +16,5 @@ app.add_middleware(
 app.include_router(menu.app)
 app.include_router(search.app)
 app.include_router(restaurant.app)
+app.include_router(order_detail.app)
+app.include_router(cancel.app)
---
+++ routers/order_detail.py
@@ -0,0 +1,22 @@
+from databases import database
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/get_order_state", tags = ["Orders"])
+async def get_order_state(account_id: str) -> dict:
+    return {
+        "data": database.system.get_account_order_state(account_id)
+    }
+
+@app.get("/get_payment", tags = ["Orders"])
+async def get_payment(account_id: str) -> dict:
+    return {
+        "data": database.system.get_account_payment_status(account_id)
+    }
+
+@app.get("/show_order_detail", tags = ["Orders"])
+async def show_order_detail(order_id: str, account_id: str) -> dict:
+    return {
+        "data": database.system.show_order_detail(order_id, account_id)
+    }
