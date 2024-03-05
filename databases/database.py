d4936720666997867fe93a118806e5f5236a4919
BigdraCo1 <pakawat.tab@gmail.com>
2024-03-05 15:09:20 +0700

    adjust importing file

--- databases/database.py
+++ databases/database.py
@@ -1,12 +1,13 @@
-from constants import account, restaurant
-from internals import controller
+from constants.restaurant  import *
+from constants.account import *
+from internals.controller import Controller
 
-system = controller.Controller([],
+system = Controller([],
                                [],
                                [])
 
-system.add_restaurant(account.restaurant_owner_account1)
-system.add_restaurant(account.restaurant_owner_account2)
-account.restaurant_owner_account1.assign_restaurant(restaurant.restaurant1)
-account.restaurant_owner_account1.assign_restaurant(restaurant.restaurant2)
-account.restaurant_owner_account2.assign_restaurant(restaurant.restaurant3)
+system.add_restaurant(restaurant_owner_account1)
+system.add_restaurant(restaurant_owner_account2)
+restaurant_owner_account1.assign_restaurant(restaurant1)
+restaurant_owner_account1.assign_restaurant(restaurant2)
+restaurant_owner_account2.assign_restaurant(restaurant3)
--- routers/restaurant.py
+++ routers/restaurant.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, HTTPException, status
-from databases import database
+from databases.database import system
 
 app = APIRouter()
 
 
 @app.delete("/{restaurant}", tags=['Delete'], status_code=status.HTTP_204_NO_CONTENT)
 async def remove_restaurant(restaurant: str):
-    if database.system.remove_restaurant(restaurant) != 'Success':
+    if system.remove_restaurant(restaurant) != 'Success':
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"There is no restaurant name : {restaurant}")
