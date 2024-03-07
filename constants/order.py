f3c5a711f6b1cabd483d4bfb34dfc235b736b22f
gooseknight <coppertrin@gmail.com>
2024-03-07 16:15:40 +0700

    del order_id

--- constants/order.py
+++ constants/order.py
@@ -5,7 +5,6 @@ from constants.restaurant import *
 from constants.food import *
 
 order1 = Order(
-    order_id = "Order_ID",
     customer = customer_account1,
     rider = rider_account1,
     restaurant_list = [restaurant4],
@@ -13,7 +12,6 @@ order1 = Order(
     order_state = "Get_Restaurant",
     payment = Payment(fried_chicken.price,"Online",restaurant4,"2021-01-01 12:00:00","P1","Paid"))
 order2 = Order(
-    order_id = "Order_ID2",
     customer = customer_account1,
     rider = rider_account1,
     restaurant_list = [restaurant4],
