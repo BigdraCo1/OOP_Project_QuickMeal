7d09a551c423606fff330ba1148e51844187e951
gooseknight <coppertrin@gmail.com>
2024-03-07 16:13:06 +0700

    add router

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import menu, search, restaurants, basket, review, general
+from routers import menu, search, restaurants, basket, review, general, order_detail, cancel
 import uvicorn
 
 app = FastAPI()
@@ -20,6 +20,8 @@ app.include_router(restaurants.app)
 app.include_router(basket.app)
 app.include_router(general.app)
 app.include_router(review.app)
+app.include_router(order_detail.app)
+app.include_router(cancel.app)
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="127.0.0.1", port=8000, log_level="info")
