0155cde29975ffdeb0cfb578bc96ee5afe6ca645
gooseknight <coppertrin@gmail.com>
2024-03-07 15:53:22 +0700

    fix controller.py

--- constants/controller.py
+++ constants/controller.py
@@ -15,7 +15,7 @@ restaurant_owner_account1.assign_restaurant(restaurant1)
 restaurant_owner_account1.assign_restaurant(restaurant2)
 restaurant_owner_account2.assign_restaurant(restaurant3)
 
-system.add_restaurant(restaurant_owner_account3)
+system.add_restaurant_account(restaurant_owner_account3)
 system.add_customer_account(customer_account1)
 system.add_rider_account(rider_account1)
 restaurant_owner_account3.assign_restaurant(restaurant4)
