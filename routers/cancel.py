2e7467e40806a506451a122530b1a24755391849
BigdraCo1 <pakawat.tab@gmail.com>
2024-03-11 18:39:44 +0700

    update cancel route

--- routers/cancel.py
+++ routers/cancel.py
@@ -2,23 +2,25 @@ from constants.controller import system
 from fastapi import APIRouter, HTTPException, status
 from utils.dependencies import user_dependency, restaurant_dependency, rider_dependency
 
-app = APIRouter( tags = ["Cancel"])
+app = APIRouter(tags=["Cancel"])
+
 
 @app.post("/cancel_by_customer/{account_id}/{order_id}")
-async def cancel_order(account_id: str, order_id: str, user : user_dependency) -> dict:
+async def cancel_order(account_id: str, order_id: str, user: user_dependency) -> dict:
     if user is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     return {"message": system.customer_cancel_order(account_id, order_id)}
 
 
-@app.post("/cancel_by_restaurant/{restaurant_account_id}/{order_id}/{food_name}/{string}")
-async def cancel_food(restaurant_account_id: str, order_id: str, food_name: str, restaurant : restaurant_dependency) -> dict:
+@app.post("/cancel_by_restaurant/{restaurant_id}/{order_id}/{food_name}")
+async def cancel_food(restaurant_id: str, order_id: str, food_name: str, restaurant: restaurant_dependency) -> dict:
     if restaurant is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-    return  {"message": system.restaurant_cancel_order(restaurant_account_id, order_id, food_name)}
+    return {"message": system.restaurant_cancel_order(restaurant_id, order_id, food_name)}
+
 
 @app.post("/cancel_by_rider/{rider_account_id}/{order_id}")
-async def cancel_rider(rider_account_id: str, order_id: str, rider : rider_dependency) -> dict:
+async def cancel_rider(rider_account_id: str, order_id: str, rider: rider_dependency) -> dict:
     if rider is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     return {"message": system.rider_cancel_order(rider_account_id, order_id)}
