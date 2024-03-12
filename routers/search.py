7f4705cf4e2c2af472f263b516341db84fa4c731
BigdraCo1 <pakawat.tab@gmail.com>
2024-03-12 11:49:08 +0700

    update search route

--- routers/search.py
+++ routers/search.py
@@ -4,7 +4,7 @@ from constants.controller import system
 app = APIRouter(prefix="/search", tags=["search"], responses={404: {"description": "Not found"}})
 
 
-@app.get("/{key}", status_code=status.HTTP_200_OK)
+@app.get("/menu_and_restaurant/{key}", status_code=status.HTTP_200_OK)
 async def show_search(key: str):
     if isinstance(system.search_menu_and_restaurant(key), str):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Restaurant or Menu with {key} not found")
