9cc1b86b7bba1ff14a1219903545fe2e1fccfb7c
Proxiermax <89377437+Proxiermax@users.noreply.github.com>
2024-03-13 20:53:12 +0700

    Update customer account pockets to 10 million

--- constants/pocket.py
+++ constants/pocket.py
@@ -1,7 +1,7 @@
 from internals.pocket import *
 
-customer_account1_pocket = Pocket(1500)
-customer_account2_pocket = Pocket(1500)
+customer_account1_pocket = Pocket(10000000)
+customer_account2_pocket = Pocket(10000000)
 rider_account1_pocket = Pocket(1500)
 restaurant_owner_account1_pocket = Pocket(1500)
 restaurant_owner_account2_pocket = Pocket(1500)
